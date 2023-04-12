d10470f5e438d3775a1c5ceaca5b7e40bcfa7d48
Francesco Bonifazi <francesco_bonifazi@hotmail.it>
2023-04-12 18:30:08 +0200

    prenotazione 1

--- b&b/FE/bandb/src/app/appCalendario/prenotazione/prenotazione.component.ts
+++ b&b/FE/bandb/src/app/appCalendario/prenotazione/prenotazione.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-prenotazione',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./prenotazione.component.css']
 })
 export class PrenotazioneComponent {
-
+  nSubmit(f: NgForm) {
+    console.log(f.value);  // { first: '', last: '' }
+    console.log(f.valid);  // false
+  }
 }
+
+
