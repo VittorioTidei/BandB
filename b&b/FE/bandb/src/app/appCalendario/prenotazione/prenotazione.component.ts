cb0451445f4a96bd9604d1ac3af499837e7a6cc8
Francesco Bonifazi <francesco_bonifazi@hotmail.it>
2023-04-19 16:21:44 +0200

    added ospiti button

--- b&b/FE/bandb/src/app/appCalendario/prenotazione/prenotazione.component.ts
+++ b&b/FE/bandb/src/app/appCalendario/prenotazione/prenotazione.component.ts
@@ -7,10 +7,37 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./prenotazione.component.css']
 })
 export class PrenotazioneComponent {
+  
+  private a = 1;
+
+  decrement(){
+    const num = document.getElementById('num') as HTMLInputElement | null;
+    
+    
+
+
+    if(this.a > 1){
+      this.a--;
+      num.innerHTML = this.a.toString();
+
+    }
+    
+  }
+
+  increment(){
+    const num = document.getElementById('num') as HTMLInputElement | null;
+  
+    if(this.a < 3){
+      this.a++;
+      num.innerHTML = this.a.toString();
+
+    }
+    
+  }
+  
+
   nSubmit(f: NgForm) {
     console.log(f.value);  // { first: '', last: '' }
     console.log(f.valid);  // false
   }
 }
-
-
