c91f81ecd01cb6a8d271b13871a967c6b3db0cc4
Francesco Bonifazi <francesco_bonifazi@hotmail.it>
2023-05-03 20:01:15 +0200

    DatePicker works with API calls

--- b&b/FE/bandb/src/app/appCalendario/prenotazione/prenotazione.component.ts
+++ b&b/FE/bandb/src/app/appCalendario/prenotazione/prenotazione.component.ts
@@ -3,7 +3,6 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { PrenotazioneService } from 'src/service/prenotazione.service';
 import { Prenotazione } from 'src/service/prenotazione';
-import { NgbDate, NgbCalendar, NgbDateStruct} from '@ng-bootstrap/ng-bootstrap';
 
 
 @Component({
@@ -11,7 +10,7 @@ import { NgbDate, NgbCalendar, NgbDateStruct} from '@ng-bootstrap/ng-bootstrap';
   templateUrl: './prenotazione.component.html',
   styleUrls: ['./prenotazione.component.css']
 })
-export class PrenotazioneComponent {
+export class PrenotazioneComponent implements OnInit{
   
   form:FormGroup;
   prenotazione: Prenotazione = new Prenotazione();
@@ -34,20 +33,13 @@ export class PrenotazioneComponent {
    }
 
 
+  ngOnInit(): void {
 
-
-   
-
-  /*checkIfDisabled(date: NgbDateStruct){
-    const d = new Date(date.year, date.month -1, date.day);
-    var prenotazioni: Prenotazione[];
     this.PrenotazioneService.getPrenotazione().subscribe(data => {
-      prenotazioni = data;
-      console.log(prenotazioni);
+      this.prenotazioni = data;
     });
-    return true;
-  }*/
-
+     
+   }
 
   decrement(){
     const num = document.getElementById('num') as HTMLInputElement | null;
@@ -70,15 +62,49 @@ export class PrenotazioneComponent {
     }
     
   }
+
+  isBefore(date1: Date, date2: Date) {
+    return date1 <= date2;
+  }
   
+  isAfter(date1: Date, date2: Date) {
+    return date1 >= date2;
+  }
+
+  isInRange(date_I: Date, date: Date, date_F: Date){
+    return (this.isAfter(date, date_I) && this.isBefore(date, date_F));
+  }
+
+
+  myFilter = (d: Date): boolean => {
+    /*
+      TRUE - DISPONIBILE
+      FALSE - DISABILITATA
+    */
+    var filtered: boolean;
+    var checked: boolean = false;
+    d = new Date(d);
+
+    this.prenotazioni.forEach(element => {
+      var data_I = new Date(element.data_inizio);
+      var data_F = new Date(element.data_fine);
+      if(!checked){
+        if(this.isInRange(data_I, d, data_F)){
+          checked = true;
+          filtered = false;
+        } else {
+          filtered = true;
+        }
+      }
+    });
+    
+    return filtered;
+  }
 
   send(){
     this.prenotazione = this.form.value;
     this.prenotazione.camera = 102;
-    this.PrenotazioneService.addPrenotazione(this.prenotazione).subscribe()
-    /*if(!this.form.valid){
-      alert("Compila coglione!")
-      return;
-    }*/
+    this.PrenotazioneService.addPrenotazione(this.prenotazione).subscribe();
+
   }
 }
