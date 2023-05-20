7d1df749ace2341fa40b96744c6be63d7dd7f670
Francesco Bonifazi <francesco_bonifazi@hotmail.it>
2023-05-20 16:17:24 +0200

    Beautify home-component.ts

--- b&b/FE/bandb/src/app/home-component/home-component.component.ts
+++ b&b/FE/bandb/src/app/home-component/home-component.component.ts
@@ -11,21 +11,17 @@ import { NgForm } from '@angular/forms';
 export class HomeComponentComponent {
 
 
-  gallery = ["../../assets/img/camere/101/101.jpeg", 
-  "../../assets/img/camere/101/101-2.jpeg", 
-  "../../assets/img/camere/101/101-3.jpeg",
-  "../../assets/img/camere/101/101-4.jpeg",
-  "../../assets/img/camere/103/103-1.jpeg",
-  "../../assets/img/camere/103/103-2.jpeg",
-  "../../assets/img/camere/104/104-2.jpeg",
-  "../../assets/img/camere/104/104-3.jpeg",
-  "../../assets/img/camere/104/104-4.jpeg",
-  "../../assets/img/camere/corridoio1.jpeg",
-  "../../assets/img/camere/corridoio2.jpeg"
- ]
-
-  onSubmit(f: NgForm) {
-    console.log(f.value);  // { first: '', last: '' }
-    console.log(f.valid);  // false
-  }
+  gallery = [
+    "../../assets/img/camere/101/101.jpeg", 
+    "../../assets/img/camere/101/101-2.jpeg", 
+    "../../assets/img/camere/101/101-3.jpeg",
+    "../../assets/img/camere/101/101-4.jpeg",
+    "../../assets/img/camere/103/103-1.jpeg",
+    "../../assets/img/camere/103/103-2.jpeg",
+    "../../assets/img/camere/104/104-2.jpeg",
+    "../../assets/img/camere/104/104-3.jpeg",
+    "../../assets/img/camere/104/104-4.jpeg",
+    "../../assets/img/camere/corridoio1.jpeg",
+    "../../assets/img/camere/corridoio2.jpeg"
+  ]
 }
