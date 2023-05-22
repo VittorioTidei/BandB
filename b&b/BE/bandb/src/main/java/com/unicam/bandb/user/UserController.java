ae4a184e8d46f8dd55162f306b6114459caa10a0
Francesco Bonifazi <francesco_bonifazi@hotmail.it>
2023-05-23 00:27:54 +0200

    Fix double / in requestMapping

--- b&b/BE/bandb/src/main/java/com/unicam/bandb/user/UserController.java
+++ b&b/BE/bandb/src/main/java/com/unicam/bandb/user/UserController.java
@@ -5,7 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("api/v1//user")
+@RequestMapping("api/v1/user")
 @CrossOrigin(origins = "http://localhost:4200/")
 public class UserController {
 
